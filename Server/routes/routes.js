1f7ebb635ee344d7a5df0581b6ef896c08a92464
UsanaseD <todiddy20@gmail.com>
2019-05-26 10:30:26 +0200

    feature(sign-up-route):add a signup route to for signup backend[Start #166253173]

---
+++ Server/routes/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const routerHelper = require('./routehelper/routerHelper');
+app.use(express.json());
+
+
+app.post('/api/v1/auth/signup', routerHelper.signup);
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
