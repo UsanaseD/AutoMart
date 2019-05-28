38258461567332e8dc28c0edae5ce8b15a10d977
UsanaseD <todiddy20@gmail.com>
2019-05-28 04:06:37 +0200

    feature(order-route):add order route[Start #166277411]

---
+++ Server/routes/routehelper/order_route_helper.js
@@ -0,0 +1,23 @@
+module.exports.order=(req,res)=>{
+    const joi=require('@hapi/joi');
+    const {orders}=require('./model/model');
+    const schema=joi.object().keys({
+        car_id:joi.number().integer(),
+        status:joi.string(),
+        old_price_offered:joi.number().integer(),
+        new_price_offered:joi.number().integer(),
+    });
+    joi.validate(req.body,schema,(err,value)=>{
+         if(err) return res.send(err.details[0].message);
+         const ordr={
+             id:orders.length+1,
+             car_id:value.car_id,
+             createdOn:new Date(),
+             status:value.status,
+             old_price_offered:value.old_price_offered,
+             new_price_offered:value.new_price_offered,
+         };
+         orders.push(ordr);
+         res.status(200).send(ordr);
+    });
+};
--- Server/routes/routes.js
+++ Server/routes/routes.js
@@ -3,12 +3,14 @@ const app = express();
 const routerHelper_signup = require('./routehelper/signup_route_helper');
 const routerHelper_login = require('./routehelper/login_route_helper');
 const routehelper_car = require('./routehelper/car_route_helper');
+const order_route_helper=require('./routehelper/order_route_helper');
 app.use(express.json());
 
 
 app.post('/api/v1/auth/signup', routerHelper_signup.signup);
 app.post('/api/v1/auth/login', routerHelper_login.login);
 app.post('/api/v1/car', routehelper_car.car);
+app.post('/api/v1/order',order_route_helper.order);
 
 
 const port = process.env.PORT || 3000;
