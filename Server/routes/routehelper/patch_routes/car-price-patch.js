a0420b29f82dc10517cfd40f0b0f2b6f8726fae2
UsanaseD <todiddy20@gmail.com>
2019-05-29 13:58:38 +0200

    feature that allows users to update price of their add

---
+++ Server/routes/routehelper/patch_routes/car-price-patch.js
@@ -0,0 +1,14 @@
+const joi=require('@hapi/joi');
+const {cars}=require('../model/model');
+module.exports.car=(req,res)=>{
+    const schema=joi.object().keys({
+        price:joi.number(),
+    });
+    joi.validate(req.body,schema,(err,value)=> {
+        if (err) return res.send(err.details[0].message);
+        const car=cars.find(car=>car.id==parseInt(req.params.id,10));
+        if(!car) return res.send('the requested Id doesnt exist');
+        car.price=value.price;
+        return res.send(car);
+    });
+};
--- Server/routes/routehelper/patch_routes/order_patch_helper.js
+++ Server/routes/routehelper/patch_routes/order_patch_helper.js
@@ -2,15 +2,15 @@ const joi = require('@hapi/joi');
 const { orders } = require('../model/model');
 
 module.exports.order = (req, res) => {
-  const schema=joi.object().keys({
-    new_price_offered:joi.number().integer(),
+  const schema = joi.object().keys({
+    new_price_offered: joi.number().integer(),
   });
-  joi.validate(req.body,schema,(err,value)=>{
+  joi.validate(req.body, schema, (err, value) => {
     if (err) return res.send(err.details[0].message);
-  const order = orders.find(order => order.id == parseInt(req.params.id, 10) && order.status == 'pending');
-  if (!order) return res.send('the stated id doesnt exist or the status is not pending');
-  order.new_price_offered = value.new_price_offered;
-  return res.send(order);
+    const order = orders.find(order => order.id == parseInt(req.params.id, 10) && order.status == 'pending');
+    if (!order) return res.send('the stated id doesnt exist or the status is not pending');
+    order.new_price_offered = value.new_price_offered;
+    return res.send(order);
 
-});
+  });
 };
