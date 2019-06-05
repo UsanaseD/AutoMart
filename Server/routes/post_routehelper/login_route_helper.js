8bae4176d29d8480de161589fc05989a08e0160c
UsanaseD <todiddy20@gmail.com>
2019-06-05 09:41:19 +0200

    chore(users-post-testing) tests the authentication of users[Starts #166462624]

--- Server/routes/post_routehelper/login_route_helper.js
+++ Server/routes/post_routehelper/login_route_helper.js
@@ -2,18 +2,13 @@ const joi = require('@hapi/joi');
 const { users } = require('../model/model');
 module.exports.login = (req, res) => {
     const schema = joi.object().keys({
-      id: joi.number().integer(),
-      firstname: joi.string(),
-      lastname: joi.string(),
       email: joi.string().email({ minDomainSegments: 2 }),
     });
   joi.validate(req.body,schema,(err,value)=>{
       if (err) return res.send(err.details[0].message);
       const user= {
-          id: value.id,
-          firstname:value.firstname,
-          lastname:value.lastname,
           email:value.email,
+          password:value.password,
       };
       users.push(user);
       res.status(200).send(user);
