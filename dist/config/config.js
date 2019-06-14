8985ebcc8254ff0f5a16b29b9e02f53301793062
UsanaseD <todiddy20@gmail.com>
2019-06-14 06:18:21 +0200

    chore(api documentation): configur swagger to describe api documentation

--- dist/config/config.js
+++ dist/config/config.js
@@ -1,1 +1,11 @@
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = {
+  PORT: 8000,
+  SECRETKEY: '0EDB8D524C2A504D7BD83F9FD7349178'
+};
+exports["default"] = _default;
